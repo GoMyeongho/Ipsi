225fe8198cb4552f9dc96700e8dba9a06bfd263f
GoMyeongho <sdx02013@ajou.ac.kr>
2024-12-31 15:44:42 +0900

    chat 부분 커밋 폴더 안 파일들은 db 구현 버전

---
+++ BackendCapstone/src/main/java/kh/BackendCapstone/dto/chat/ChatMsgDto.java
@@ -0,0 +1,20 @@
+package kh.BackendCapstone.dto.chat;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMsgDto {
+    public enum MsgType {
+        ENTER, TALK, CLOSE
+    }
+    private MsgType type;
+    private Long id;
+    private String roomId;
+    private String profile;
+    private String nickName;
+    private String sender;
+    private String msg;
+    private String active;
+}
---
+++ BackendCapstone/src/main/java/kh/BackendCapstone/entity/chat/Chat.java
@@ -0,0 +1,50 @@
+package kh.BackendCapstone.entity.chat;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "chat")
+public class Chat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "sender")
+    private String sender;
+
+    @Column(name = "profile")
+    private String profile;
+
+    @Column(name = "nickName")
+    private String nickName;
+
+    @Column(name = "msg")
+    private String msg;
+
+    @Column(name = "sent_at")
+    private LocalDateTime sentAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    @JsonIgnore
+    private ChatRoom chatRoom;
+
+    @ElementCollection
+    @CollectionTable(
+            name = "chat_member",
+            joinColumns = @JoinColumn(name = "chat_id")
+    )
+    @Column(name = "member")
+    private List<String> members;
+
+    private String active;
+}
