0ef033c776046c9e086a767e9428bb143d3c9c3d
waterkong01 <gioremo615@gmail.com>
2025-01-02 15:46:57 +0900

    250102_15h45m

--- BackendCapstone/src/main/java/kh/BackendCapstone/repository/CommentRepository.java
+++ BackendCapstone/src/main/java/kh/BackendCapstone/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-	public List<Comment> findByBoard(TextBoard textBoard);
+	List<Comment> findByTextBoard(TextBoard textBoard);
 }
