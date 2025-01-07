3afff4960029ae8ec5697431b52072746ebd2d7d
GoMyeongho <sdx02013@ajou.ac.kr>
2025-01-07 10:27:27 +0900

    프론트 파이어베이스

--- frontend-capstone/src/api/Firebase.js
+++ frontend-capstone/src/api/Firebase.js
@@ -1,7 +1,8 @@
-import firebase from "../firebase";
-import "firebase/compat/storage"
+// api/firebase.js
+import firebase from "firebase/compat/app";
+import "firebase/compat/storage";  // Firebase Storage를 사용하기 위한 import
 
-
+// Firebase 초기화
 const firebaseConfig = {
 	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
@@ -11,6 +12,22 @@ const firebaseConfig = {
 	appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
-firebase.initializeApp(firebaseConfig);
-export const storage = firebase.storage()
-export default firebase;
+if (!firebase.apps.length) {
+	firebase.initializeApp(firebaseConfig);  // Firebase 초기화
+} else {
+	firebase.app();  // 이미 초기화된 앱이 있으면 사용
+}
+
+// 파일 업로드 함수
+export const uploadFileToFirebase = (file, filePath) => {
+	const storageRef = firebase.storage().ref();  // Firebase 스토리지 참조
+	const fileRef = storageRef.child(filePath);  // 업로드할 파일 경로 설정
+	
+	return fileRef.put(file)  // 파일 업로드
+		.then(() => {
+			return fileRef.getDownloadURL();  // 업로드가 끝나면 다운로드 URL 반환
+		})
+		.catch((error) => {
+			throw new Error(error.message);  // 에러 처리
+		});
+};
